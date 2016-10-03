a96c543b511bfd111fb6eb1d9e3208337562d121
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-10-03 10:19:33 +0200

    Updated

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,14 @@ RUN apt-get install -y git curl unzip
 RUN git clone https://github.com/SWI-Prolog/swish.git
 RUN curl http://www.swi-prolog.org/download/swish/swish-bower-components.zip > swish/swish-bower-components.zip
 RUN cd swish && unzip swish-bower-components.zip
-ADD daemon.pl swish/daemon.pl
-RUN useradd -ms /bin/false swish
 
-CMD cd swish && swipl daemon.pl	--port=3050 --user=swish --no-fork
+VOLUME /data
+
+COPY swish.sh swish.sh
+EXPOSE 3050
+
+ENV SWISH_HOME /swish
+ENV SWISH_DATA /data
+WORKDIR=${SWISH_DATA}
+
+ENTRYPOINT ["bash", "/swish.sh"]
---
+++ swish.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Start script for the SWISH docker
+#
+# This script is started in /data.  SWISH is in /swish
+
+reconf=no
+config=
+start=--no-fork
+
+usage()
+{ echo "Usage: docker run [docker options] swish [swish options]"
+  echo "swish options:"
+  echo "  --interactive          Run Prolog toplevel (requires -it)"
+  echo "  --help                 Display usage"
+}
+
+while [ ! -z "$1" ]; do
+  case "$1" in
+    --interactive)	start=--interactive
+			shift
+			;;
+    --help)		usage
+			exit 0
+			;;
+  esac
+done
+
+if [ ! -f daemon.pl ]; then
+
+fi
+
+if [ ! -f settings.db ]; then
+  cp /settings.db .
+fi
+
+for dir in storage; do
+  mkdir -p $dir
+  chown daemon $dir
+done
+
+for file in ; do
+  if [ ! -f $file ]; then
+    touch $file
+  fi
+  chown daemon $file
+done
+
+./daemon.pl --port=3050 --user=daemon $start
